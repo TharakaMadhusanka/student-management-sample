3c4ae9aca00d50ae42e2a56e0bd8ed8ae9e1af50
TharakaMadhusanka <tharakalearn@gmail.com>
2020-06-27 12:19:34 +0530

    Added School Record Update Method

--- api-project/controller/school.controller.js
+++ api-project/controller/school.controller.js
@@ -3,11 +3,33 @@ var config = require('../utility/config');
 var contextService = require('../service/connectdb.service');
 
 
+// Method to Get List Of Schools
 exports.GetListOfSchools = async (_req, _res) => {
     const limit = (_req.query.noOfRows) ? _req.query.noOfRows : 10;
     const offSet = (_req.query.pageNumber) ? (_req.query.pageNumber - 1) * limit : 0;
     const searchFor = (_req.query.searchFor) ? _req.query.searchFor : '';
     let listOfRecords = [];
+    try 
+    {
+        listOfRecords = await contextService.ExecuteQuery('CALL GetListOfSchools(' 
+                                + limit + ',' 
+                                + offSet + ',"'
+                                + searchFor + '")');
+        
+        return listOfRecords[0];
+    }
+    catch(error) {
+        console.log(error);
+        throw new Error(error);
+        
+    }
+}
+
+// Method to Insert, Update, Delete School Record
+// If Flag = 1 -> Insert
+// If Flag = 3 -> Delete
+// If Flag = 2 -> Update
+exports.UpdateSchool = async (_req, _res) => {
     try 
     {
         listOfRecords = await contextService.ExecuteQuery('CALL GetListOfSchools(' 
